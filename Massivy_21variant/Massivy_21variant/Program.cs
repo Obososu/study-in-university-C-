82b1116a49397e045c899bbd7c7eb1bc6736492e
Nikita Ponomarev <54476476+Obososu@users.noreply.github.com>
2020-10-08 15:31:52 +0300

    Add files via upload

---
+++ Massivy_21variant/Massivy_21variant/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Massivy_21variant_1zadanie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество учеников:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите баллы по дисциплинам:");
+            Console.WriteLine("(если баллов нет - поставьте ноль)");
+            int[][] Students = new int[n][];
+            for (int i = 0; i<n; i++)
+            {
+                Students[i] = new int[8];
+            }
+            for (int i = 0; i < n; i++)
+            {
+                
+                string qq = "Введите оценки " + (i + 1) + " ученика через пробел";
+                Console.WriteLine(qq);
+                string[] str = Console.ReadLine().Split(new char[] { ' ', '\n', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+                for (int j = 0; j < 8; j++)
+                {
+                    Students[i][j] = Convert.ToInt32(str[j]);
+                }
+            }
+            for (int i = 0; i < n; i++)
+            {
+                int sum = 0;
+                int predm = 0;
+                for (int j = 0; j < 8; j++)
+                {
+                    
+                    if (Students[i][j] == 0)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        sum = sum + Students[i][j];
+                        predm++;
+                    }
+                    
+                }
+                double x = (double)sum / (double)predm;
+
+                Console.WriteLine("Средний балл " + (i+1) + " ученика равен: " + Math.Round(x, 2));
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Massivy_21variant_3zadanie/Massivy_21variant_3zadanie/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Massivy_21variant_3zadanie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Сколько слов хотите ввести?");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            string[] S21 = new string[n];
+            Console.WriteLine("Вводите слова через новую сроку");
+            for (int i = 0; i < n; i++)
+            {
+                S21[i] = Console.ReadLine();
+            }
+            for (int i = 0; (i < n - 1)&(i % 2 == 0); i+=4)
+            {
+                string rr = S21[i + 2];
+                for (int j = i + 2; j < n; j++)
+                {
+                    S21[j] = S21[i];
+                    S21[i] = rr;
+                    break;
+                }
+            }
+            Console.WriteLine("Выводим слова:");
+            for(int i = 0; i < n; i++)
+            {
+                Console.WriteLine(S21[i]);
+            }
+
+
+
+
+
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
