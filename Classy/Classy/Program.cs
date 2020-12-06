174bc096ea3a59d737cad67a810e03e57079dbce
Nikita Ponomarev <54476476+Obososu@users.noreply.github.com>
2020-12-06 12:52:19 +0300

    Add files via upload

---
+++ Classy/Classy/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classy
+{
+    class Program
+    {
+        static void Main(string[] args) { }
+           class Chel
+           { 
+            public void Запуск()
+            { Console.WriteLine("ku"); }
+           }
+    }       
+    
+}
+
